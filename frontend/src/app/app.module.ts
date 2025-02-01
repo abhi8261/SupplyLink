956c63c46191106b99a81962d9258e3ad571c878
username <wecp-dummy@gmail.com>
2025-02-01 03:55:33 +0000

    day 19

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';  
+import { SupplierArrayComponent } from './supplylink/components/supplierarray/supplierarray.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
--- frontend/src/app/supplylink/components/supplier/supplier.component.ts
+++ frontend/src/app/supplylink/components/supplier/supplier.component.ts
@@ -1,5 +1,44 @@
+import { Component, OnInit } from "@angular/core";
+import { Supplier } from "../../types/Supplier";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { Observable, of } from "rxjs";
+@Component({
+    selector: 'app-supplier',
+    templateUrl: "./supplier.component.html",
+    styleUrls: ['./supplier.component.scss']
+  })
+export class SupplierComponent implements OnInit {
+    supplierForm: FormGroup;
+    suppliers : Supplier []=[];
+    supplierSuccess$ : any;
+    supplierError$ : any;
+    
+    supplier: Supplier = new Supplier(1, "Jessica Alba", "jessica@gmail.com", "7368289682", "california", "jessica", "July@12221", "USER");
+    constructor(private formBuilder: FormBuilder){
 
-export class SupplierComponent  {
+    }
+    ngOnInit(): void {
+        this.supplierForm=this.formBuilder.group({
+            name :['', [Validators.required]],
+            email: ['', Validators.required],
+            username:['',Validators.required],
+            password:['', Validators.required]
+        })
+    }
+    onSubmit(){
+        if(this.supplierForm.valid){
+            const newSupplier= this.supplierForm.value;
+            this.suppliers.push(newSupplier);
+            this.supplierSuccess$ ='Supplier added successfully.';
+            this.supplierError$='';
+        
+        }else{
+            this.supplierError$='Invalid data.';
+            this.supplierSuccess$=''
+        }
+    }
 
-  
+
+       
 }
+
