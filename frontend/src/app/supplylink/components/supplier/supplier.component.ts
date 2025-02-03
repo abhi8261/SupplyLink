0c1336d241801139b2e25af2a913e7d9f2e8b432
username <wecp-dummy@gmail.com>
2025-02-03 13:36:47 +0000

    day21

--- frontend/src/app/supplylink/components/supplier/supplier.component.ts
+++ frontend/src/app/supplylink/components/supplier/supplier.component.ts
@@ -1,44 +1,126 @@
+
+
+
+
+
+
 import { Component, OnInit } from "@angular/core";
 import { Supplier } from "../../types/Supplier";
-import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { AbstractControl, FormBuilder, FormGroup, ValidationErrors, Validators } from "@angular/forms";
 import { Observable, of } from "rxjs";
+
+
 @Component({
     selector: 'app-supplier',
     templateUrl: "./supplier.component.html",
-    styleUrls: ['./supplier.component.scss']
-  })
-export class SupplierComponent implements OnInit {
-    supplierForm: FormGroup;
-    suppliers : Supplier []=[];
-    supplierSuccess$ : any;
-    supplierError$ : any;
-    
-    supplier: Supplier = new Supplier(1, "Jessica Alba", "jessica@gmail.com", "7368289682", "california", "jessica", "July@12221", "USER");
-    constructor(private formBuilder: FormBuilder){
-
-    }
-    ngOnInit(): void {
-        this.supplierForm=this.formBuilder.group({
-            name :['', [Validators.required]],
-            email: ['', Validators.required],
-            username:['',Validators.required],
-            password:['', Validators.required]
-        })
-    }
-    onSubmit(){
-        if(this.supplierForm.valid){
-            const newSupplier= this.supplierForm.value;
-            this.suppliers.push(newSupplier);
-            this.supplierSuccess$ ='Supplier added successfully.';
-            this.supplierError$='';
-        
-        }else{
-            this.supplierError$='Invalid data.';
-            this.supplierSuccess$=''
+        styleUrls: ['./supplier.component.scss']
+      })
+    export class SupplierComponent implements OnInit {
+        supplierForm:FormGroup;
+        supplierErrors$:Observable<string>;
+        supplierSuccess$:Observable<string>;
+        constructor(private fb:FormBuilder){
+          this.supplierErrors$=of('');
+          this.supplierSuccess$=of('');
         }
+
+    ngOnInit(): void {
+    this.supplierForm = this.fb.group({
+      
+      supplierName: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
+      
+      phone: ['', [Validators.required]],
+      address: [''],
+      username: ['', [Validators.required, this.noSpecialCharacters]],
+      password: ['', [Validators.required, Validators.minLength(8)]],
+      role: ['', [Validators.required]]
+    });
+  }
+
+  noSpecialCharacters (controls : AbstractControl) : ValidationErrors | null {
+    const patternData = /^[a-zA-Z0-9]+$/;
+    if(patternData.test(controls.value)){
+      return {invalid : false};
     }
+    return {invalid : true};
+  }
+  onSubmit(): void {
+    if (this.supplierForm.valid) {
+      const newSupplier = this.supplierForm.value;
+      //this.suppliers.push(newSupplier);
+      this.supplierSuccess$=of('Supplier added successfully');
+      this.supplierErrors$=of('');
+
+    }else{
+        this.supplierErrors$=of('Form is invalid. Please fix the errors and try again.');
+        this.supplierSuccess$=of('');
+    }
+}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import { Component, OnInit } from "@angular/core";
+// import { Supplier } from "../../types/Supplier";
+// import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+// import { Observable, of } from "rxjs";
+// @Component({
+//     selector: 'app-supplier',
+//     templateUrl: "./supplier.component.html",
+//     styleUrls: ['./supplier.component.scss']
+//   })
+// export class SupplierComponent implements OnInit {
+//     supplierForm: FormGroup;
+//     suppliers : Supplier []=[];
+//     supplierSuccess$ : any;
+//     supplierError$ : any;
+    
+//     supplier: Supplier = new Supplier(1, "Jessica Alba", "jessica@gmail.com", "7368289682", "california", "jessica", "July@12221", "USER");
+//     constructor(private formBuilder: FormBuilder){
+
+//     }
+//     ngOnInit(): void {
+//         this.supplierForm=this.formBuilder.group({
+//             name :['', [Validators.required]],
+//             email: ['', Validators.required],
+//             username:['',Validators.required],
+//             password:['', Validators.required]
+//         })
+//     }
+//     onSubmit(){
+//         if(this.supplierForm.valid){
+//             const newSupplier= this.supplierForm.value;
+//             this.suppliers.push(newSupplier);
+//             this.supplierSuccess$ ='Supplier added successfully.';
+//             this.supplierError$='';
+        
+//         }else{
+//             this.supplierError$='Invalid data.';
+//             this.supplierSuccess$=''
+//         }
+//     }
 
 
        
-}
+// }
 
--- frontend/src/app/supplylink/components/warehouse/warehouse.component.ts
+++ frontend/src/app/supplylink/components/warehouse/warehouse.component.ts
@@ -1,5 +1,48 @@
 
+
+
+import { Component } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { Observable, of } from "rxjs";
+
+@Component ({
+    selector : 'app-warehouse',
+    template : './warehouse.component.html',
+    styleUrls : ['./warehouse.component.scss']
+})
+
 export class WarehouseComponent {
- 
+
+  warehouseForm: FormGroup;
+  warehouseError$: Observable<string>;
+  warehouseSuccess$: Observable<string>;
+
+  constructor(private fb: FormBuilder) {
+    this.warehouseError$ = of('');
+    this.warehouseSuccess$ = of('');
+  }
+
+  ngOnInit(): void {
+    this.warehouseForm = this.fb.group({
+      //warehouseId : ['', Validators.required],
+      supplierId: ['', [Validators.required]],
+      warehouseName: ['', Validators.required],
+      location : ['', [Validators.required]],
+      capacity: [0, [Validators.required, Validators.min(0)]]
+    });
+  }
+
+  onSubmit(): void {
+    if (this.warehouseForm.valid) {
+      const newSupplier = this.warehouseForm.value;
+      console.log('New Supplier:', newSupplier);
+  
+      this.warehouseSuccess$ = of('Supplier added successfully!');
+      this.warehouseError$ = of('');
+    } else {
+      this.warehouseError$ = of('Form is invalid. Please fix the errors and try again.');
+      this.warehouseSuccess$ = of('');
+    }
+  }
 
 }
